d90527776745d80570d1696b97ea5a614eba73ac
Marcin Szepczyński <szepczynski@gmail.com>
2020-05-23 16:10:10 +0200

    fix SmsApiTransportFactory

--- SmsapiTransportFactory.php
+++ SmsapiTransportFactory.php
@@ -2,6 +2,7 @@
 
 namespace Symfony\Component\Notifier\Bridge\Smsapi;
 
+use Symfony\Component\Notifier\Exception\IncompleteDsnException;
 use Symfony\Component\Notifier\Exception\UnsupportedSchemeException;
 use Symfony\Component\Notifier\Transport\AbstractTransportFactory;
 use Symfony\Component\Notifier\Transport\Dsn;
@@ -18,14 +19,17 @@ class SmsapiTransportFactory extends AbstractTransportFactory
     public function create(Dsn $dsn): TransportInterface
     {
         $scheme = $dsn->getScheme();
-        $accountSid = $this->getUser($dsn);
-        $authToken = $this->getPassword($dsn);
-        $from = $dsn->getOption('from');
+        $authToken = ltrim($dsn->getPath(), '/');
         $host = 'default' === $dsn->getHost() ? null : $dsn->getHost();
+        $from = $dsn->getOption('from');
         $port = $dsn->getPort();
 
+        if (!$id) {
+            throw new IncompleteDsnException('Missing path (maybe you haven\'t update the DSN when upgrading from 5.0).', $dsn->getOriginalDsn());
+        }
+
         if ('smsapi' === $scheme) {
-            return (new SmsapiTransport($accountSid, $authToken, $from, $this->client, $this->dispatcher))->setHost($host)->setPort($port);
+            return (new SmsapiTransport($authToken, $from, $this->client, $this->dispatcher))->setHost($host)->setPort($port);
         }
 
         throw new UnsupportedSchemeException($dsn, 'smsapi', $this->getSupportedSchemes());
